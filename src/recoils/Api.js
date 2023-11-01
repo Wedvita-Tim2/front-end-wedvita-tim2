080e6b388e7362ae4a1ced1aa1fea77a8c189c32
Daniar27 <daniarmiftah@gmail.com>
2023-11-01 16:41:25 +0700

    <add>Add copy link invitation

--- src/recoils/Api.js
+++ src/recoils/Api.js
@@ -4,3 +4,8 @@ export const apiBackend= atom({
     key: 'backendApi',
     default: 'http://localhost:8000/',
 });
+
+export const urlFront = atom({
+    key: 'urlFront',
+    default: 'http://localhost:3000'
+})
